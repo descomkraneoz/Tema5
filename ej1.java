40ebc4244c9ade5ddc7651423efabc068100014c
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-12-05 20:09:55 +0100

    Add files via upload

---
+++ ej1.java
@@ -0,0 +1,13 @@
+public class ej1 {
+    public static void main(String[] args) {
+
+        System.out.println("Ejercicio 1");
+        int[] val = {0, 1, 2, 3};
+
+        int sum =val[0]+val[1]+val[2]+val[3];
+
+        System.out.println( "Sum of all numbers = " + sum );
+
+
+    }
+}
