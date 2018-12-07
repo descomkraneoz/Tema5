0532aaf6643ebd37d7bb394913095391bb92873b
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-12-07 13:03:35 +0100

    Add files via upload

--- ej5.java
+++ ej5.java
@@ -1,7 +1,7 @@
 public class ej5 {
     public static void main(String[] args) {
         int[] val = {0, 1, 2, 3};
-        int temp;
+        int temp = 0;
 
         System.out.println( "Original Array: "
                 + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
@@ -18,16 +18,28 @@ public class ej5 {
 
         //segunda forma mas larga con int
 
-        temp=val[3];
+        /*temp=val[3];
         val[3]=val[0];
         val[0]=temp;
         temp=val[1];
         val[1]=val[2];
-        val[2]=temp;
+        val[2]=temp;*/
 
 
+        //tercera forma con un metodo
+
+        val = reverse(val);
 
         System.out.println( "Reversed Array: "
                 + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    }
+
+    public static int[] reverse(int[] lista){
+        int[] resultado = new int[lista.length];
+
+        for (int i = 0, j=resultado.length-1; i <lista.length ; i++, j--) {
+            resultado[j]=lista[i];
+        }
+        return  resultado;
     }
 }
