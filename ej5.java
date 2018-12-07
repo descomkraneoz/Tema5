a3cdee5ab4cf80e2ff762a1dc2addfb662db1ec4
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-12-07 10:22:07 +0100

    Add files via upload

--- ej5.java
+++ ej5.java
@@ -8,11 +8,24 @@ public class ej5 {
 
         // reverse the order of the numbers in the array
 
-        for (int i = 0; i <val.length/2 ; i++) {
+        //primera forma con for
+
+        /*for (int i = 0; i <val.length/2 ; i++) {
             temp=val[i];
             val[i]=val[val.length-i-1];
             val[val.length-i-1]=temp;
-        }
+        }*/
+
+        //segunda forma mas larga con int
+
+        temp=val[3];
+        val[3]=val[0];
+        val[0]=temp;
+        temp=val[1];
+        val[1]=val[2];
+        val[2]=temp;
+
+
 
         System.out.println( "Reversed Array: "
                 + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
